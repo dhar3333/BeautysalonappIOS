1994014e2205a66ed271f6d1a6fcf377806f02e7
Dharshini Selvaraj <dharshiniselvaraj3333@gmail.com>
2022-04-01 21:37:35 -0400

    first commit

---
+++ salonapp/registration.swift
@@ -0,0 +1,29 @@
+//
+//  registration.swift
+//  salonapp
+//
+//  Created by Dharshini Selvaraj on 2022-03-31.
+//
+
+import UIKit
+
+class registration: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
